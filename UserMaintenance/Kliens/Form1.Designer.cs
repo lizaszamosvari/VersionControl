06b575b38de9c2d47e7944f737566f8de204b4f7
Lenovo <edina.fulop@stud.uni-corvinus.hu>
2024-04-21 23:10:03 +0200

    Felhasználói felület alapja

--- UserMaintenance/Kliens/Form1.Designer.cs
+++ UserMaintenance/Kliens/Form1.Designer.cs
@@ -28,12 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            Cancel = new Button();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 20;
+            listBox1.Location = new Point(26, 66);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(150, 284);
+            listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(26, 24);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // Cancel
+            // 
+            Cancel.DialogResult = DialogResult.Cancel;
+            Cancel.Location = new Point(394, 373);
+            Cancel.Name = "Cancel";
+            Cancel.Size = new Size(94, 29);
+            Cancel.TabIndex = 2;
+            Cancel.Text = "Mégse";
+            Cancel.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(545, 373);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 3;
+            button1.Text = "Mentés";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(441, 201);
+            label1.Name = "label1";
+            label1.Size = new Size(67, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Elérhető:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(534, 201);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 20);
+            label2.TabIndex = 5;
+            label2.Text = "50 db";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(418, 286);
+            label3.Name = "label3";
+            label3.Size = new Size(119, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Újonnan eladott:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(543, 283);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(51, 27);
+            textBox2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(600, 286);
+            label4.Name = "label4";
+            label4.Size = new Size(27, 20);
+            label4.TabIndex = 8;
+            label4.Text = "db";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(Cancel);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private TextBox textBox1;
+        private Button Cancel;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
     }
 }
