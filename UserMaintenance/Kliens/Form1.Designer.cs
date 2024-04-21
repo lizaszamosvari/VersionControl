09cd1553a088cb8b3fc3df5146f0d57bf67f06a3
Lenovo <edina.fulop@stud.uni-corvinus.hu>
2024-04-21 23:35:54 +0200

    Biztosan mentené albak

--- UserMaintenance/Kliens/Form1.Designer.cs
+++ UserMaintenance/Kliens/Form1.Designer.cs
@@ -73,6 +73,7 @@
             button1.TabIndex = 3;
             button1.Text = "Mentés";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // label1
             // 
--- UserMaintenance/Kliens/Form1.cs
+++ UserMaintenance/Kliens/Form1.cs
@@ -6,5 +6,12 @@ namespace Kliens
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+
+        }
     }
 }
