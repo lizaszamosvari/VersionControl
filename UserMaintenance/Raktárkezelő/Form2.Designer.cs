d40022dc50da4232d28ca8b00c91eca9a4e35ddd
Lenovo <edina.fulop@stud.uni-corvinus.hu>
2024-04-23 00:04:18 +0200

    Form2 design

--- UserMaintenance/Raktárkezelő/Form2.Designer.cs
+++ UserMaintenance/Raktárkezelő/Form2.Designer.cs
@@ -36,36 +36,42 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(58, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(169, 16);
+            this.label1.Size = new System.Drawing.Size(252, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Biztosan szeretné menteni?";
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Black;
             this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button1.Location = new System.Drawing.Point(25, 130);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(41, 99);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(81, 33);
             this.button1.TabIndex = 1;
             this.button1.Text = "Mégse";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.Yellow;
             this.button2.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button2.Location = new System.Drawing.Point(258, 130);
+            this.button2.Location = new System.Drawing.Point(247, 99);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(81, 33);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Mentés";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Text = "Ok";
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(388, 175);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
