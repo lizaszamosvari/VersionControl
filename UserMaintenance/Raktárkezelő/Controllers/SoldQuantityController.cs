3025cdffe5551ac73acae6c846bcdcbc3eb23801
Lenovo <edina.fulop@stud.uni-corvinus.hu>
2024-04-23 20:52:28 +0200

    class SoldQuantityController

---
+++ UserMaintenance/Raktárkezelő/Controllers/SoldQuantityController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Raktárkezelő.Controllers
+{
+    public class SoldQuantityController
+    {
+        public bool ModifySoldQuantity(string quantity)
+        {
+            try
+            {
+                
+
+
+                if (!ValidateQuantity(quantity.ToString()))
+                {
+                    MessageBox.Show("Érvénytelen ár. Ne adj meg negatív, nullával kezdődő vagy nem egész értékeket");
+                    return false;
+                }
+                return true;
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show($"Nem sikerült a módosítás: {err.Message}");
+                //throw;
+                return false;
+            }
+
+
+        }
+
+        public bool ValidateQuantity(string quantity)
+        {
+            Regex regex = new Regex(@"^(0|[1-9]\d*)$");
+            return regex.IsMatch(quantity);
+        }
+    }
+}
