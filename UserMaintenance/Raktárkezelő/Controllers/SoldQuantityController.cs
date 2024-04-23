0287c6a1161426c172f85370bcff7934dc307962
Lenovo <edina.fulop@stud.uni-corvinus.hu>
2024-04-23 21:41:38 +0200

    .

--- UserMaintenance/Raktárkezelő/Controllers/SoldQuantityController.cs
+++ UserMaintenance/Raktárkezelő/Controllers/SoldQuantityController.cs
@@ -21,7 +21,7 @@ namespace Raktárkezelő.Controllers
 
                 if (!ValidateQuantity(quantity.ToString()))
                 {
-                    MessageBox.Show("Érvénytelen darabszám. Ne adj meg negatív, nullával kezdődő vagy nem egész értékeket");
+                    MessageBox.Show("Érvénytelen darabszám. Ne adj meg negatív, nullával kezdődő vagy nem egész értékeket, és ne hagyd üresen");
                     return false;
                 }
                 return true;
