70a0c98cb740927621072e5d478738868e118599
Liza Bella Szamosvári <liza.szamosvari@stud.uni-corvinus.hu>
2024-04-24 10:28:49 +0200

    unittest

---
+++ UserMaintenance/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("661d88f1-e617-46b9-9cd5-be51a8c1da2d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UserMaintenance/UnitTest/soldquantitycontrollertest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using Raktárkezelő.Controllers;
+
+namespace UnitTest
+{
+    public class soldquantitycontrollertest
+    {
+
+        [Test,
+         TestCase("3"),
+         TestCase("5")
+         ]
+        public void TestModifySpldQuantity(string quantity)
+        {
+            //Arrange
+            var productcontroller = new SoldQuantityController();
+
+            // Act
+            var actualResponse = productcontroller.ModifySoldQuantity(quantity);
+
+            // Assert
+            ClassicAssert.True(actualResponse);
+        }
+
+
+
+
+        [Test,
+        TestCase("5,5", false),
+        TestCase("-10", false),
+        TestCase("03", false),
+        TestCase("7", true)
+        ]
+
+
+        public void TestValidatQuantity(string Quantity, bool expectedResult)
+        {
+            //Arrange
+            var quantitycontroller = new SoldQuantityController();
+
+            // Act
+            var actualResult = quantitycontroller.ValidateQuantity(Quantity);
+
+            //Assert
+            ClassicAssert.AreEqual(expectedResult, actualResult);
+
+        }
+
+    }
+}
